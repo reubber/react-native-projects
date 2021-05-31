5a0b1d9926e5c750a4119781d46641d0c263740d
reubber <reubbers@alu.ufc.br>
2021-05-31 00:10:46 -0300

    config and style input

--- guess-number-game/screens/StartGameScreen.js
+++ guess-number-game/screens/StartGameScreen.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
 
 import Card from '../components/Card'
+import Input from '../components/Input'
 import Colors from '../constants/colors'
 
 export default function StartGameScreen(props) {
@@ -10,14 +11,14 @@ export default function StartGameScreen(props) {
       <Text style={styles.title}> Start a new Game </Text>
       <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
-        <TextInput/>
+        <Input style={styles.input} blurOnSubmit autoCorrect={false} keyboardType="number-pad" maxLength={2} />
       
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
-            <Button style={styles.button} title="Reset" onPress={()=>{}} color={Colors.primaryStrong} />
+            <Button style={styles.button} title="Reset" onPress={()=>{}} color={Colors.secondary} />
           </View>
           <View style={styles.button}>
-            <Button title="Confirm" onPress={()=>{}} color={Colors.secondary}/>
+            <Button title="Confirm" onPress={()=>{}} color={Colors.primaryStrong}/>
           </View>
         </View>
       </Card>
@@ -49,5 +50,9 @@ const styles = StyleSheet.create({
   },
   button: {
     width: 100
+  },
+  input: {
+    width: 50,
+    textAlign: 'center'
   }
 })
