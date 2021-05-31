c418c1a0382384c4bbb2c61b2ec83ee2f08b6b93
reubber <reubbers@alu.ufc.br>
2021-05-31 14:35:28 -0300

    Adicionando recursos do jogo: logica e validação**

--- guess-number-game/screens/GameScreen.js
+++ guess-number-game/screens/GameScreen.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import React, {useState, useRef } from 'react'
+import { StyleSheet, Text, View, Button, Alert } from 'react-native'
 
 import NumberContainer from '../components/NumberContainer'
 import Card from '../components/Card'
@@ -18,14 +18,36 @@ const generateRandomBetween = (min, max, exclude) =>  {
 
 
 export default function GameScreen(props) {
-  const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 99, props.userChoice))
+  const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 100, props.userChoice))
+  const currentLow = useRef(1)
+  const currentHigh = useRef(100)
+  
+   const nextGuessHandler = direction => {
+    if(
+      (direction ==='lower' && currentGuess < props.userChoice) || 
+      (direction === 'greater' && currentGuess > props.userChoice)
+    ) {
+      Alert.alert('Dont lie!', 'you know that this is wrong...', [{text: 'Sorry', style: 'cancel'}
+    ])
+    return
+    }
+    if (direction === 'lower') {
+      currentHigh.current = currentGuess
+    } else {
+      currentLow.current = currentGuess
+    }
+
+    const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess)
+    setCurrentGuess(nextNumber)
+  }
+
   return (
     <View style={styles.screen}>
       <Text>Oponnent's Guess</Text>
       <NumberContainer>{currentGuess}</NumberContainer>
       <Card style={styles.buttonContainer}>
-        <Button title="LOWER" onPress={()=>{}}/>
-        <Button title="GREATER" onPress={()=> {}} />
+        <Button title="LOWER" onPress={nextGuessHandler.bind(this, 'lower')}/>
+        <Button title="GREATER" onPress={nextGuessHandler.bind(this, 'greater')} />
       </Card>
     </View>
   )
