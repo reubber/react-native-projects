4e740547e016d97400c6571ae6e761458929df2d
Reubber Sousa <reubbers@alu.ufc.br>
2021-06-23 15:44:47 -0300

    add Dimensions API

--- guess-number-game/screens/GameScreen.js
+++ guess-number-game/screens/GameScreen.js
@@ -1,11 +1,12 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect} from 'react';
 import {
   View,
   Text,
   StyleSheet,
   Alert,
   ScrollView,
-  FlatList
+  FlatList,
+  Dimensions 
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
@@ -76,11 +77,22 @@ const GameScreen = props => {
     ]);
   };
 
+  // if (Dimensions.get('window').height> 600) {
+  //   return <View>...</View>
+  // }
+
+  let listContainerStyle = styles.listContainer;
+
+
+  if(Dimensions.get('window').width<350) {
+    listContainerStyle = styles.listContainerBig
+  }
+
   return (
     <View style={styles.screen}>
       <Text style={DefaultStyles.title}>Opponent's Guess</Text>
       <NumberContainer>{currentGuess}</NumberContainer>
-      <Card style={styles.buttonContainer}>
+      <Card style={Dimensions.get('window').height > 600 ? styles.buttonContainer : styles.buttonContainerSmall}>
         <MainButton onPress={nextGuessHandler.bind(this, 'lower')}>
           <Ionicons name="md-remove" size={24} color="white" />
         </MainButton>
@@ -112,13 +124,24 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: 'row',
     justifyContent: 'space-around',
-    marginTop: 20,
+    marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
     width: 400,
+    maxWidth: '90%'
+  },
+  buttonContainerSmall: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
+    width: 300,
     maxWidth: '90%'
   },
   listContainer: {
     flex: 1,
-    width: '60%'
+    width: Dimensions.get('window').width > 350 ? '60%' : '80%'
+  },
+  listContainerBig: {
+    flex: 1,
+    width: '80%'
   },
   list: {
     flexGrow: 1,
--- guess-number-game/screens/StartGameScreen.js
+++ guess-number-game/screens/StartGameScreen.js
@@ -6,7 +6,8 @@ import {
   Button,
   TouchableWithoutFeedback,
   Keyboard,
-  Alert
+  Alert,
+  Dimensions
 } from 'react-native';
 
 import Card from '../components/Card';
@@ -116,8 +117,9 @@ const styles = StyleSheet.create({
     fontFamily: 'open-sans-bold'
   },
   inputContainer: {
-    width: 300,
-    maxWidth: '80%',
+    width: '80%',
+    maxWidth: '95%',
+    minWidth: 300,
     alignItems: 'center'
   },
   buttonContainer: {
@@ -127,7 +129,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15
   },
   button: {
-    width: 100
+    width: Dimensions.get('window').width / 4
   },
   input: {
     width: 50,
