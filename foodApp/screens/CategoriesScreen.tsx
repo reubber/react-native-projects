f5f3968698915627606c635d5e0793f10eb0d624
Reubber Sousa <reubbers@alu.ufc.br>
2021-11-13 20:34:41 -0300

    passing & reading params Upon Navigation

--- foodApp/screens/CategoriesScreen.tsx
+++ foodApp/screens/CategoriesScreen.tsx
@@ -1,22 +1,62 @@
 import React from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { 
+  StyleSheet, 
+  Text, View, 
+  Button, 
+  FlatList, 
+  TouchableOpacity, 
+  Platform
+} from 'react-native'
+import Colors from '../constants/Colors';
+
+import { CATEGORIES, Category } from '../data/dummy-data'
+
 
 export function CategoriesScreen (props: any) {
   const { navigation } = props;
+  
+  const renderGridItem = (itemData : any) => {
+    return (
+      <TouchableOpacity
+        style={styles.gridItem}
+        onPress={ ()=> {
+          navigation.navigate({routeName: 'CategoryMeals', params: {
+            categoryId: itemData.item.id
+          }})
+      }}> 
+        <View>
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+
+
   return (
-    <View style={styles.screen}>
-      <Text>The Categories Screen</Text>
-      <Button title="go to Meals" onPress={()=>{
-        navigation.navigate({routeName: 'CategoryMeals'});
-      }} />
-    </View>
+    <FlatList
+      keyExtractor={(item, index) => item.id }
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+    />
   )
 }
+
+CategoriesScreen.navigationOptions = {
+  headerTitle: 'Meal Categories',
+  headerStyle: {
+    backgroundColor: Platform.OS === 'android' ? Colors.primaryColor: ''
+  },
+  headerTintColor: Platform.OS === 'android' ? 'white': Colors.primaryColor
+};
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  gridItem: {
+    flex: 1,
   }
 })
--- foodApp/screens/CategoryMealsScreen.tsx
+++ foodApp/screens/CategoryMealsScreen.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Button, StyleSheet, Text, View } from 'react-native'
+import { CATEGORIES } from '../data/dummy-data';
 
 export function CategoryMealsScreen (props: { navigation: any; }) {
   const { navigation } = props;
+  const catId = navigation.getParam('categoryId');
+
+
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId)
+
   return (
     <View style={styles.screen}>
       <Text>The Categories Meals Screen</Text>
+      <Text>{selectedCategory.title}</Text>
       <Button title="go to Meals detail" onPress={()=>{
         navigation.navigate({routeName: 'MealDetail'});
       }} />
