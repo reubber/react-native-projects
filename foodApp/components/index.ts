9d6d923387d232bd6841699abc24a7531984bb56
reubber de sousa <reubbers@gmail.com>
2021-12-29 13:56:37 -0300

    Render lista de Meals por categoria

--- foodApp/components/index.ts
+++ foodApp/components/index.ts
@@ -1,3 +1,4 @@
 import { fromEventPattern } from 'rxjs';
 
 export * from './categories';
+export * from './meal-item';
--- foodApp/screens/CategoryMealsScreen.tsx
+++ foodApp/screens/CategoryMealsScreen.tsx
@@ -5,7 +5,7 @@ import { NavigationStackScreenProps } from 'react-navigation-stack';
 
 import { CATEGORIES, MEALS } from '../data/dummy-data';
 import { IMeal } from '../models/meal';
-
+import { MealItem } from '../components';
 
 export function CategoryMealsScreen (props: { navigation: any; }) {
   const { navigation } = props;
@@ -13,21 +13,27 @@ export function CategoryMealsScreen (props: { navigation: any; }) {
 
   const renderMealItem = (itemData: IMeal) => {
     return (
-      <View>
-        <Text>{itemData.item.title}</Text>
-      </View>
+      <MealItem 
+        title={itemData.item.title}
+        image={itemData.item.imageUrl}
+        onSelectMeal={()=>{}} 
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+      />
     )
   } 
   
-
   const displayMeals = MEALS.filter(
     meals => meals.categoryIds.indexOf(catId) >= 0)
 
   return (
     <View style={styles.screen}>
-      <FlatList data={displayMeals}
+      <FlatList
+        data={displayMeals}
         keyExtractor={(item, _) => item.id}
         renderItem={renderMealItem}
+        style={{width: '100%'}}
       />
     </View>
   )
