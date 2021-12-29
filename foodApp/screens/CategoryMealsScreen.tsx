5364a92a32216f6016fe951a437b83627f255437
reubber de sousa <reubbers@gmail.com>
2021-12-29 10:39:15 -0300

    carregando meals por categorias

--- foodApp/screens/CategoryMealsScreen.tsx
+++ foodApp/screens/CategoryMealsScreen.tsx
@@ -1,27 +1,33 @@
 import React from 'react'
-import { Button, Platform, StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, View, Text } from 'react-native'
 import Colors from '../constants/Colors';
 import { NavigationStackScreenProps } from 'react-navigation-stack';
 
-import { CATEGORIES } from '../data/dummy-data';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
+import { IMeal } from '../models/meal';
+
 
 export function CategoryMealsScreen (props: { navigation: any; }) {
   const { navigation } = props;
   const catId = navigation.getParam('categoryId');
 
+  const renderMealItem = (itemData: IMeal) => {
+    return (
+      <View>
+        <Text>{itemData.item.title}</Text>
+      </View>
+    )
+  } 
+  
 
-  const selectedCategory = CATEGORIES.find(cat => cat.id === catId)
+  const displayMeals = MEALS.filter(
+    meals => meals.categoryIds.indexOf(catId) >= 0)
 
   return (
     <View style={styles.screen}>
-      <Text>The Categories Meals Screen</Text>
-      <Text>{selectedCategory.title}</Text>
-      <Button title="go to Meals detail" onPress={()=>{
-        navigation.navigate({routeName: 'MealDetail'});
-      }} />
-      <Button title="Go back" onPress={()=> {
-        navigation.goBack();
-      }}
+      <FlatList data={displayMeals}
+        keyExtractor={(item, _) => item.id}
+        renderItem={renderMealItem}
       />
     </View>
   )
