8fdecfa726f7d7c1dd74b7ebc4d831b520ca6c88
reubber de sousa <reubbers@gmail.com>
2022-05-29 09:04:39 -0300

    init expo project

---
+++ foodApp-nav6/models/meal.ts
@@ -0,0 +1,63 @@
+export interface IMeal {
+  item: {
+    id: string;
+    categoryIds: Array<string>;
+    title: string;
+    affordability: string;
+    complexity: string;
+    imageUrl: string;
+    duration: number;
+    ingredients: Array<any>;
+    steps: Array<string>;
+    isGlutenFree: boolean;
+    isVegan:boolean;
+    isVegetarian: boolean;
+    isLactoseFree: boolean;
+  }
+}
+class Meal {
+  id: string;
+  categoryIds: Array<string>;
+  title: string;
+  affordability: string;
+  complexity: string;
+  imageUrl: string;
+  duration: number;
+  ingredients: Array<any>;
+  steps: Array<string>;
+  isGlutenFree: boolean;
+  isVegan:boolean;
+  isVegetarian: boolean;
+  isLactoseFree: boolean;
+  constructor(
+    id: string,
+    categoryIds: Array<string>,
+    title: string,
+    affordability: string,
+    complexity: string,
+    imageUrl: string,
+    duration: number,
+    ingredients: Array<any>,
+    steps: Array<string>,
+    isGlutenFree: boolean,
+    isVegan: boolean,
+    isVegetarian: boolean,
+    isLactoseFree: boolean
+  ) {
+    this.id = id;
+    this.categoryIds = categoryIds;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.ingredients = ingredients;
+    this.steps = steps;
+    this.duration = duration;
+    this.complexity = complexity;
+    this.affordability = affordability;
+    this.isGlutenFree = isGlutenFree;
+    this.isVegan = isVegan;
+    this.isVegetarian = isVegetarian;
+    this.isLactoseFree = isLactoseFree;
+  }
+}
+
+export default Meal;
