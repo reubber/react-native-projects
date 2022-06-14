c810ef4de6c5a42ec288a90276ebe194fc2f0da0
reubber de sousa <reubbers@gmail.com>
2022-06-14 13:42:08 -0300

    feat: add react-navigation6

--- foodApp-nav6/App.tsx
+++ foodApp-nav6/App.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import { CategoriesScreen } from './screens/CategoriesScreen';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
     <React.Fragment>
-      <StatusBar style='light'/>
-      <CategoriesScreen />
+      <StatusBar style='auto'/>
+      <NavigationContainer>
+        <CategoriesScreen />
+      </NavigationContainer>
     </React.Fragment>
   );
 }
